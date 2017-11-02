fd7dd0c02cfd0d6a2467e4e5bb43b979ff579c63
nanda <nandapuspitarana@gmail.com>
2017-11-02 21:31:43 +0700

    image slide

---
+++ app/js/slideresponsive.js
@@ -0,0 +1,23 @@
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 50,
+      // init: false,
+      breakpoints: {
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        }
+      }
+    });
