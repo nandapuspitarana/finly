945fe02fb0f3891912e81af41f31833947c0f48b
nanda <nandapuspitarana@gmail.com>
2017-11-03 10:31:49 +0700

    responsive, editing hover

--- app/js/slideresponsive.js
+++ app/js/slideresponsive.js
@@ -1,19 +1,24 @@
-    var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container', {
       slidesPerView: 4,
       spaceBetween: 50,
+      loop: true,
       // init: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
       breakpoints: {
         1024: {
           slidesPerView: 4,
-          spaceBetween: 40,
+          spaceBetween: 30,
         },
         768: {
           slidesPerView: 3,
-          spaceBetween: 30,
+          spaceBetween: 20,
         },
         640: {
           slidesPerView: 2,
-          spaceBetween: 20,
+          spaceBetween: 10,
         },
         320: {
           slidesPerView: 1,
